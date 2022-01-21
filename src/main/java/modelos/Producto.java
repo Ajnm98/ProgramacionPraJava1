9dec80ce616e059b50414aba6a708a220ac3f832
Ajnm98 <anunezmorejon@safareyes.es>
2022-01-21 19:13:36 +0100

    Inicio

---
+++ src/main/java/modelos/Producto.java
@@ -0,0 +1,14 @@
+package modelos;
+
+import java.time.LocalDate;
+
+public class Producto {
+
+    private Integer identificador;
+    private String codigo;
+    private String descripcion;
+    private LocalDate fechaCaducidad;
+
+
+
+}
